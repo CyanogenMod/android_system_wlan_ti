446aa7f3a3944c64e482173f23a10dcc165c7367
Dmitry Shmidt <dimitrysh@google.com>
2009-09-01 10:07:30 -0700

    Fix fsm NULL pointer crash if not initialized
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- wilink_6_1/utils/fsm.c
+++ wilink_6_1/utils/fsm.c
@@ -105,6 +105,7 @@ TI_STATUS fsm_Create(TI_HANDLE				hOs,
 	{
 		return TI_NOK;
 	}
+	os_memoryZero(hOs, (*pFsm), sizeof(fsm_stateMachine_t));
 
 	/* allocate memory for FSM matrix */
 	(*pFsm)->stateEventMatrix = (fsm_Matrix_t)os_memoryAlloc(hOs, MaxNoOfStates * MaxNoOfEvents * sizeof(fsm_actionCell_t));
@@ -113,7 +114,8 @@ TI_STATUS fsm_Create(TI_HANDLE				hOs,
 		os_memoryFree(hOs, *pFsm, sizeof(fsm_stateMachine_t));
 		return TI_NOK;
 	}
-
+	os_memoryZero(hOs, (*pFsm)->stateEventMatrix, 
+		(MaxNoOfStates * MaxNoOfEvents * sizeof(fsm_actionCell_t)));
 	/* update pFsm structure with parameters */
 	(*pFsm)->MaxNoOfStates = MaxNoOfStates;
 	(*pFsm)->MaxNoOfEvents = MaxNoOfEvents;
@@ -265,6 +267,9 @@ TI_STATUS fsm_Event(fsm_stateMachine_t		*pFsm,
 	*currentState = pFsm->stateEventMatrix[(*currentState * pFsm->ActiveNoOfEvents) + event].nextState;
 
 	/* activate transition function */
+	if( !(*pFsm->stateEventMatrix[(oldState * pFsm->ActiveNoOfEvents) + event].actionFunc) ) {
+		return(TI_NOK);
+	}
 	status = (*pFsm->stateEventMatrix[(oldState * pFsm->ActiveNoOfEvents) + event].actionFunc)(pData);
 
 	return status;
